1929b86c923eff54ac4e2f9fa91e0d0ded481b65
Phyo Thiha Kyaw <phyothihacodes@gmail.com>
2025-02-10 01:44:50 +0700

    feat: add logging
    Add Serilog with console and seq sinks

--- src/SmartUni.PublicApi/Program.cs
+++ src/SmartUni.PublicApi/Program.cs
@@ -1,12 +1,16 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddOpenApi();
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
 
 var app = builder.Build();
+app.UseSerilogRequestLogging();
+app.MapGet("/", () => Log.Information("Web server started!"));
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+if (app.Environment.IsDevelopment()) app.MapOpenApi();
 
 app.UseHttpsRedirection();
 app.Run();
