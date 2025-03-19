42b6dcdda65073d79ce4c774b4e63f40ad5c2933
Myo Kyaw Htun <myokyawhtun083914@gmail.com>
2025-03-20 01:25:36 +0630

    feat: Messaging Development
    1.add necessary model
    2.develop functions for messaging with signalR and save message to db
    3.add new end point to select total message of sender

---
+++ src/SmartUni.PublicApi/Features/Message/ChatMessage.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartUni.PublicApi.Features.Message
+{
+
+    public class ChatMessage
+    {
+        
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        
+        public required string SenderId { get; set; } = string.Empty;
+
+       
+        public required string ChatRoomId { get; set; } = string.Empty;
+
+      
+        public required string Content { get; set; } = string.Empty;
+
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    }
+
+
+}
--- src/SmartUni.PublicApi/Persistence/SmartUniDbContext.cs
+++ src/SmartUni.PublicApi/Persistence/SmartUniDbContext.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore.Metadata;
 using SmartUni.PublicApi.Common.Domain;
 using SmartUni.PublicApi.Features.Allocation;
+using SmartUni.PublicApi.Features.Message;
 using SmartUni.PublicApi.Features.Staff;
 using SmartUni.PublicApi.Features.Student;
 using SmartUni.PublicApi.Features.Tutor;
@@ -15,6 +16,9 @@ namespace SmartUni.PublicApi.Persistence
         public DbSet<Staff> Staff { get; set; }
         public DbSet<Student> Student { get; set; }
         public DbSet<Allocation> Allocations { get; set; }
+        public DbSet<ChatMessage> ChatMessage { get; set; }
+        //public DbSet<ChatRoom> ChatRoom { get; set; }
+        public DbSet<ChatParticipant> ChatParticipant { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -34,10 +38,10 @@ namespace SmartUni.PublicApi.Persistence
             modelBuilder.Entity<IdentityRole<Guid>>().ToTable("asp_net_role");
             modelBuilder.Entity<IdentityUserRole<Guid>>().ToTable("asp_net_user_role");
             modelBuilder.Entity<IdentityRoleClaim<Guid>>().ToTable("asp_net_role_claim");
-
             PasswordHasher<BaseUser> hasher = new();
             Guid userId = Guid.Parse("8edcd6b3-0489-4766-abed-284e8945f13d");
             Guid adminId = Guid.Parse("8fb67550-b862-4a0f-94fd-c212f5e35802");
+            
             modelBuilder.Entity<BaseUser>().HasData(new BaseUser
             {
                 Id = userId,
